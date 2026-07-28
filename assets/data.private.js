6e369de3d00d9b321109ac52c425405cc1bd15b8
Aaron Cates <cates.aaron@gmail.com>
2026-07-28 11:04:44 -0500

    Updated my app features

---
+++ assets/data.private.js
@@ -0,0 +1,81 @@
+class Data {
+  constructor(context) {
+    this.context = context;
+  }
+
+  getCurrentTalk() {
+    return this.getUpcomingTalks()[0];
+  }
+
+  getUpcomingTalks() {
+    return [
+      {
+        title: "Billionaires in Space",
+        code: "astronaut",
+        startTime: "2026-10-01T17:00:00.038Z",
+        speakers: [
+          {
+            name: "Aaron",
+            phoneNumber: "+15019083119",
+          },
+        ],
+      },
+      {
+        title: "Taco Tuesdays should be every day",
+        code: "taco",
+        startTime: "2026-10-02T17:00:00.038Z",
+        speakers: [
+          {
+            name: "Aaron",
+            phoneNumber: "+15019083119",
+          },
+        ],
+      },
+    ];
+  }
+
+  getTalkByCode(code) {
+    const talks = this.getUpcomingTalks();
+    return talks.find((talk) => talk.code === code);
+  }
+
+  addRegistration(code, phoneNumber) {
+    // NOOP
+    return true;
+  }
+
+  async getRegistrants(talk) {
+    // TODO: This needs to come from an external data source
+    // NOT the messages log
+    const client = this.context.getTwilioClient();
+    const messages = await client.messages.list({
+      to: this.context.TWILIO_PHONE_NUMBER,
+    });
+    return messages
+      .filter((msg) => {
+        const action = this.parseInput(msg.body);
+        return action.command === "join" && action.code === talk.code;
+      })
+      .map((msg) => {
+        return { phoneNumber: msg.from };
+      });
+  }
+
+  parseInput(input) {
+    // eg: join tacos
+    const action = {
+      input,
+    };
+    const normalized = input.trim().toLowerCase();
+    const parts = normalized.split(/\s+/);
+    if (parts.length === 2) {
+      action.command = parts[0];
+      action.code = parts[1];
+    }
+    return action;
+  }
+}
+
+module.exports = {
+  Data,
+};
--- functions/incoming-call.js
+++ functions/incoming-call.js
@@ -1,7 +1,25 @@
+const assets = Runtime.getAssets();
+const { Data } = require(assets["/data.js"].path);
+
 exports.handler = (context, event, callback) => {
-    const twiml = new Twilio.twiml.VoiceResponse();
-    // TODO: this should be name of current talk
-    twiml.dial().conference("sample-conference-name");
-    console.log(`TwiML is ${twiml}`);
-    return callback(null, twiml);
+  const twiml = new Twilio.twiml.VoiceResponse();
+  const data = new Data(context);
+  const talk = data.getCurrentTalk();
+  if (talk !== undefined) {
+    twiml.dial().conference(
+      {
+        muted: true,
+        beep: false,
+        startConferenceOnEnter: false,
+      },
+      talk.code
+    );
+  } else {
+    // TODO: Build a voice representation of upcoming talks
+    twiml.say(
+      "There is no talk currently. Send us a text for a schedule of upcoming talks"
+    );
+  }
+  console.log(`TwiML is ${twiml}`);
+  return callback(null, twiml);
 };
